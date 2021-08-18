746779a070ef0d228717cc2a8eb01b31b2a46a0e
Lucas Andrade <89050515+landrade-s@users.noreply.github.com>
2021-08-18 20:01:17 -0300

    Add files via upload

---
+++ Aula 01.js
@@ -0,0 +1,14 @@
+//console.log('Olá mundo!');
+//prompt("Digite seu nome ");
+
+//let usuario = 'LS';
+//console.log(usuario)
+
+let aluno = prompt('Digite o seu nome');
+let turma = prompt('Digite sua turma');
+
+console.log("Olá, "+aluno+"! bem vindo(a) a turma "
++turma+"!");
+
+//let fruta = prompt('Digite o nome da fruta');
+//console.log(fruta);
---
+++ Aula 02.js
@@ -0,0 +1,62 @@
+// let varTexto = "Isso é uma string";
+// let varNumero = 30;
+// let varBoolean = 9>2;
+
+// let numero1 = 15
+// let numero2 = 10
+// let comparar = numero1 > numero2
+
+// console.log(varTexto);
+// console.log(varNumero);
+// console.log(varBoolean);
+//console.log(comparar);
+
+// typeof varBoolean
+
+// let arrayNome = ["João","Duda","Matheus","Simone"]
+// let arrayNumero = [10,20,30,40,50]
+// let arrayMisto = ["Jezimiel",false,500]
+
+// console.log(arrayNome)
+// console.log(arrayNumero[0])
+// console.log(arrayMisto[1])
+
+// typeof arrayNome
+
+// var a = "Turma C7";
+// var b = a;
+// console.log(b);
+
+// var c = d = "String"
+// console.log(c)
+// console.log(d)
+
+// const constTest = "Isso é uma  const"
+// console.log(constTest)
+
+// OPERADORES
+
+let soma = 6 + 9;
+console.log(soma)
+console.log("Boa "+"Noite!")
+
+let numero1 = 35
+let numero2 = 10
+
+let somanumeros = numero1 + numero2
+console.log(somanumeros)
+
+let subtrainumeros = numero1 - numero2
+console.log(subtrainumeros)
+
+let multiplicanumero = numero1 * numero2
+console.log(multiplicanumero)
+
+let dividenumero = numero1 / numero2
+console.log(dividenumero)
+
+let comparaNumero = 6 !== 6
+let comparaTexto = "Texto" === "Texto"
+console.log(comparaNumero)
+console.log(comparaTexto)
+
